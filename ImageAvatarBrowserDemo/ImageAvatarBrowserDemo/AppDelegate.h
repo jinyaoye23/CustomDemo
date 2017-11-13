d21fcdf10c39dcb85e22b1bf9b3447d71caeeaa5
jinyaoye23 <jinyaoye23@126.com>
2017-11-13 16:34:25 +0800

    添加当张图片浏览模式demo
    添加当张图片浏览模式demo

---
+++ ImageAvatarBrowserDemo/ImageAvatarBrowserDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImageAvatarBrowserDemo
+//
+//  Created by Yao on 2017/11/13.
+//  Copyright © 2017年 叶进耀. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
